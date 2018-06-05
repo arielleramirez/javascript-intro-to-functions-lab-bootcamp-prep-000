852207004f8124d6f26f02aead9cd9e3071c2ae6
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-05 19:13:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string){
 
 function sayHiToGrandma (string) {
   if (string.toLowerCase() === string) {
-    return( "I can't hear you!")
-   }if (string.toUpperCase() === string){
-      return ("YES INDEED!" )
+    return "I can't hear you!"
+   }if else (string.toUpperCase() === string){
+      return "YES INDEED!" 
     } else {
-        return ("I love you, too")
-   }
+        return "I love you, too"
+   
     
 }
